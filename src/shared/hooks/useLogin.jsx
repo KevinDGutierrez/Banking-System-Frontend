2d990a9996f307f47e7c32e1771d52cf4a6de9f9
PabloCastillo10 <pablocastillo4821@gmail.com>
2025-06-11 11:06:45 -0600

    clase 11/06/2025

--- src/shared/hooks/useLogin.jsx
+++ src/shared/hooks/useLogin.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
 import {useNavigate} from "react-router-dom"
-import { login } from "../../services/api";
+import { login as loginService } from "../../services/api";
+import Swal from "sweetalert2";
 
 
 export const login = async () => {
     const navigate = useNavigate()
-    const [loginError, setLoginError] = useState(null);
-    const [loginSucces, setLoginSucces] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleLogin = async (data) => {
+    const handleLogin = async (usernameOrEmail, password) => {
+        setIsLoading(true)
         try {
-            const response = await loginSucces (data)
-            const userDetails = response;
+            const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(usernameOrEmail);
+            const response = await loginService({
+                [isEmail ? "email" : "username"] : usernameOrEmail,
+                password
+            });
+            const {userDetails} = response.data;
 
             localStorage.setItem("user", JSON.stringify(userDetails));
+
+            await Swal.fire({
+                icon : 'success',
+                title : 'Inicio exitoso',
+                text : 'Inicio de sesion exitosamente!!',
+                timer : 3000,
+                showConfirmButton : false
+            })
+            navigate ('/dashboard')
         } catch (error) {
-            setLoginError(error.message);
+            const Errormsg = error.response?.data
+
+            Swal.fire({
+                icon : 'error',
+                title : 'Error',
+                text : Errormsg?.error || Errormsg?.msg || 'Ocurrio un error'
+            })
+        } finally {
+            setIsLoading(false)
         }
     } 
+    return {
+        login,handleLogin
+    }
 }
