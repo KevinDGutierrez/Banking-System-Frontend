ecbc74d3a4f61fed594e0e7d97ece202889a95f8
PabloCastillo10 <pablocastillo4821@gmail.com>
2025-06-17 11:53:35 -0600

    12

--- src/components/AccountBanking/AccountClient.jsx
+++ src/components/AccountBanking/AccountClient.jsx
@@ -42,7 +42,7 @@ const AccountClient = () => {
             entidadBancaria: '',
             saldo: 0
         });
-        await handleGetAccountBankingUser(); // Refresh the list
+        await handleGetAccountBankingUser(); 
     };
 
     if (isLoading) {
@@ -144,7 +144,7 @@ const AccountClient = () => {
                                 >
                                     <option value="">Seleccione un banco</option>
                                     {banking.map((bank) => (
-                                            <option key={bank._id} value={bank._id}>
+                                            <option key={bank.name} value={bank.name}>
                                                 {bank.name}
                                             </option>
                                         ))}
