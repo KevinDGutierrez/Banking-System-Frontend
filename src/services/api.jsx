383820d9ed306d215c2e47045faf87502a626fc7
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-09-19 09:10:28 -0600

    Arreglos - Vercel

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { logout } from "../shared/hooks/useLogout"
 
 const apiClient = axios.create({
-    baseURL: 'https://banking-system-backend-production-79ac.up.railway.app/',
+    baseURL: 'https://banking-system-omega-ten.vercel.app/',
     timeout: 5000
 })
 
