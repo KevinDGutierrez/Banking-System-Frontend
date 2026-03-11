e35725036068a65355218a8923c41cad2ff58be0
IanAlfaroSantos <ialfasa2020@gmail.com>
2026-03-11 07:48:52 -0600

    Actualización

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { logout } from "../shared/hooks/useLogout"
 
 const apiClient = axios.create({
-    baseURL: 'https://banking-system-859528286543.europe-west1.run.app/',
+    baseURL: 'banking-system-backend-production.up.railway.app/',
     timeout: 5000
 })
 
